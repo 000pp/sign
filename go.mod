2cfb58b7d4e0691731c8ac5a0e1729ce05628c43
opps3c <opps3c@gmail.com>
2025-08-20 21:49:51 -0300

    new: added ldap signing check support

--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.24
 toolchain go1.24.6
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
 	github.com/akamensky/argparse v1.4.0 // indirect
 	github.com/fatih/color v1.18.0 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.8-0.20250403174932-29230038a667 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.11 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jfjallid/go-smb v0.6.6 // indirect
 	github.com/jfjallid/gofork v1.7.6 // indirect
 	github.com/jfjallid/gokrb5/v8 v8.5.1 // indirect
--- src/protocol/ldap.go
+++ src/protocol/ldap.go
@@ -1,7 +1,95 @@
 package protocol
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"strings"
 
-func StartLDAP(target_address string) {
-	fmt.Println(target_address)
+	"github.com/go-ldap/ldap/v3"
+)
+
+type LDAPStatus struct {
+	Protocol string
+	Signing  string
+}
+
+func StartLDAP(target_address string) LDAPStatus {
+	status := checkLDAP(target_address)
+	
+	if status.Protocol != "" {
+		fmt.Printf("%s:389 - %s\n", target_address, status.Signing)
+		return status
+	}
+
+	statusTLS := checkLDAPS(target_address)
+	if statusTLS.Protocol != "" {
+		fmt.Printf("%s:636 - %s\n", target_address, statusTLS.Signing)
+	}
+	
+	return statusTLS
+}
+
+func checkLDAP(target_address string) LDAPStatus {
+	address := fmt.Sprintf("ldap://%s:389", target_address)
+	conn, err := ldap.DialURL(address)
+	if err != nil {
+		log.Printf("LDAP connection failed: %v", err)
+		return LDAPStatus{}
+	}
+	defer conn.Close()
+
+	err = conn.UnauthenticatedBind("")
+	if err != nil {
+		if signingErrorHandler(err) {
+			return LDAPStatus{Protocol: "LDAP", Signing: "Enabled and required"}
+		}
+		return LDAPStatus{Protocol: "LDAP", Signing: "Disabled"}
+	}
+
+	if supportsSigningOptional(target_address) {
+		return LDAPStatus{Protocol: "LDAP", Signing: "Enabled, but not required"}
+	}
+	
+	return LDAPStatus{Protocol: "LDAP", Signing: "Disabled"}
+}
+
+func supportsSigningOptional(target_address string) bool {
+	address := fmt.Sprintf("ldap://%s:389", target_address)
+	conn, err := ldap.DialURL(address)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+
+	err = conn.StartTLS(&tls.Config{InsecureSkipVerify: true})
+	
+	if err != nil {return false}
+
+	return true
+}
+
+func checkLDAPS(target_address string) LDAPStatus {
+	address := fmt.Sprintf("ldaps://%s:636", target_address)
+	conn, err := ldap.DialURL(address, ldap.DialWithTLSConfig(&tls.Config{InsecureSkipVerify: true}))
+
+	if err != nil {return LDAPStatus{}
+}
+	defer conn.Close()
+
+	return LDAPStatus{Protocol: "LDAPS", Signing: "Enabled and required"}
+}
+
+func signingErrorHandler(err error) bool {
+	errorMessage := strings.ToLower(err.Error())
+	keywords := []string{
+		"signing required",
+		"authentication required", 
+		"bind required",
+		"security required",
+		"confidentiality required",
+	}
+	
+	for _, keyword := range keywords {if strings.Contains(errorMessage, keyword) {return true}}
+	return false
 }
